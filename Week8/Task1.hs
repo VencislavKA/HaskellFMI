0f2ba0f61acdbf3d2822a49405335e2f554423d6
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2024-06-01 23:03:47 +0300

    Task1 Week6

--- Week8/Task1.hs
+++ Week8/Task1.hs
@@ -1,23 +1,47 @@
 import Data.List
 main::IO()
 main = do 
-    print $ 123
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 0 1 == [[1]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 1 == [[1, 2], [1, 3]]
-    -- print $ simplePaths [(1, [2, 3, 4]), (2, [3, 4]), (3, []), (4, [])] 1 1 == [[1,2],[1,3],[1,4]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 2 1 == [[1, 2, 3], [1, 2, 4]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 2 == [[2,3],[2,4]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3]), (3, []), (4, [])] 1 2 == [[2,3]]
+    print $ isNeighbour 1 2 [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] == True
+    print $ isNeighbour 2 1 [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] == False
+
+    print $ allPoints [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] == [1,2,3,4]
+
+    print $ addNeighbour [1] [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] == [[1,2],[1,3]]
+    print $ addNeighbour [1,2] [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] == [[1,2,3],[1,2,4]]
+
+    print $ addNeighbours [[1]] [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])]
+    print $ addNeighbours (addNeighbours [[1]] [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])]) [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])]
+
+    print $ addNeighbours [[1]] [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 0 1 == [[1]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 1 == [[1, 2], [1, 3]]
+    print $ simplePaths [(1, [2, 3, 4]), (2, [3, 4]), (3, []), (4, [])] 1 1 == [[1,2],[1,3],[1,4]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 2 1 == [[1, 2, 3], [1, 2, 4]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 2 == [[2,3],[2,4]]
+    print $ simplePaths [(1, [2, 3]), (2, [3]), (3, []), (4, [])] 1 2 == [[2,3]]
 
 type Node = Int
 type Graph = [(Node, [Node])]
 type Path = [Node]
 
--- format :: Graph -> [(Int,Int)]
--- format graph = 
+simplePaths :: Graph -> Int -> Node -> [Path]
+simplePaths graph len point = helper len [[point]]
+ where
+    helper :: Int -> [Path] -> [Path]
+    helper 0 res = res
+    helper len res = helper (len - 1) (addNeighbours res graph)
 
-getConnected :: Graph -> Int -> [Int]
-getConnected graph parent = snd $ head $ filter (\(par,ch) -> par == parent) graph
+allPoints :: Graph -> [Node]
+allPoints = map (fst)
 
--- simplePaths :: Graph -> Int -> Int -> [[Int]]
--- simplePaths graph k n = 
+isNeighbour :: Node -> Node -> Graph -> Bool
+isNeighbour parent neighbour graph = elem neighbour $ snd $ head $ filter (\(idx,nei) -> idx == parent) graph
+
+addNeighbours :: [Path] -> Graph -> [Path]
+addNeighbours lst graph = concat $ map (\x -> addNeighbour x graph) $ lst
+
+addNeighbour :: Path -> Graph -> [Path]
+addNeighbour lst graph = helper lst $ allPoints graph
+ where
+    helper :: Path -> [Node] -> [Path]
+    helper lst points = map (\x -> lst ++ [x]) $ filter (\x -> isNeighbour (last lst) x graph) points
